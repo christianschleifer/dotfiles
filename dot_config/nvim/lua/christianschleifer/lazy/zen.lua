0e1175a945c976318d792903eab492722d1e2011
Christian Schleifer <chr.schleifer@gmail.com>
2025-08-15 06:11:28 +0200

    Make zen window slightly wider

--- dot_config/nvim/lua/christianschleifer/lazy/zen.lua
+++ dot_config/nvim/lua/christianschleifer/lazy/zen.lua
@@ -1,7 +1,11 @@
 return {
 	"folke/zen-mode.nvim",
 	config = function()
-		require("zen-mode").setup({})
+		require("zen-mode").setup({
+			window = {
+				width = 150,
+			},
+		})
 		vim.keymap.set("n", "<leader>z", function()
 			require("zen-mode").toggle()
 		end, { desc = "Zen Mode" })
