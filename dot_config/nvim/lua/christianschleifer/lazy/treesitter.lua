12806a1623e10ad8feb9c3495ca9d883a34b205e
Christian Schleifer <chr.schleifer@gmail.com>
2025-12-26 14:08:35 +0100

    Update treesitter config

--- dot_config/nvim/lua/christianschleifer/lazy/treesitter.lua
+++ dot_config/nvim/lua/christianschleifer/lazy/treesitter.lua
@@ -1,33 +1,9 @@
 -- Highlight, edit, and navigate code
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"json",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-				"rust",
-				"python",
-			},
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		})
+		require("nvim-treesitter").setup({})
 	end,
 }
