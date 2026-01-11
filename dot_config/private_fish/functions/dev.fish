fca080b9fd9afc9d35bd3989a5992dc3f8127f77
Christian Schleifer <chr.schleifer@gmail.com>
2026-01-11 08:52:21 +0100

    Remove dashboard

--- dot_config/private_fish/functions/dev.fish
+++ dot_config/private_fish/functions/dev.fish
@@ -1,10 +1,11 @@
 function dev
     # Create session, name first window 'main'
     tmux new-session -d -s default -n main
-    
+
     # Create 'term' window in the background (-d) so focus stays on 'main'
     tmux new-window -d -t default -n term
-    
+
+
     # Attach
     tmux attach-session -t default
 end
