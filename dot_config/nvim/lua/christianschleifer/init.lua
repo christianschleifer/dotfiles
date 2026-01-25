6d7289a1cfb021c07f2e396abdeec7050a744661
Christian Schleifer <chr.schleifer@gmail.com>
2026-01-25 17:40:36 +0100

    Enable syntax highlighting

--- dot_config/nvim/lua/christianschleifer/init.lua
+++ dot_config/nvim/lua/christianschleifer/init.lua
@@ -12,3 +12,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "markdown_inline", "java" },
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
