02792f5f2408258c6b2705610b0afe2968617c33
Christian Schleifer <chr.schleifer@gmail.com>
2026-01-25 17:40:49 +0100

    Setup markdown formatting

--- dot_config/nvim/lua/christianschleifer/lazy/autoformat.lua
+++ dot_config/nvim/lua/christianschleifer/lazy/autoformat.lua
@@ -33,12 +33,10 @@ return {
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
-			-- Conform can also run multiple formatters sequentially
 			python = { "black" },
-			-- You can use 'stop_after_first' to run the first available formatter from the list
-			-- javascript = { "prettierd", "prettier", stop_after_first = true },
 			json = { "jq" },
-			markdown = { "prettier" },
+			markdown = { "prettier", "injected" },
+			java = { "google-java-format" },
 		},
 		formatters = {
 			prettier = {
