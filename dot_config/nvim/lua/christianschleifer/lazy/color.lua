7a22062f6eae9bb468914fc74dc6fe81f0f39d13
Christian Schleifer <cschleif@amazon.com>
2026-09-13 18:31:28 +0200

    Switch Neovim theme to Gruvbox

--- dot_config/nvim/lua/christianschleifer/lazy/color.lua
+++ dot_config/nvim/lua/christianschleifer/lazy/color.lua
@@ -1,8 +1,8 @@
 return {
-	"rebelot/kanagawa.nvim",
+	"ellisonleao/gruvbox.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme kanagawa")
+		vim.cmd("colorscheme gruvbox")
 	end,
 }
